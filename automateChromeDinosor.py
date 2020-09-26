902f65f546d13720518b8de80052d4dc415da3ff
deepak <deepakjha6210@gmail.com>
2020-09-27 00:32:47 +0530

    Deleting jarvis file and adding alspping bird game

--- automateChromeDinosor.py
+++ automateChromeDinosor.py
@@ -10,13 +10,13 @@ def hit(key):
 def isCollide(data):
     # Draw the rectangle for birds
     for i in range(300, 415):
-        for j in range(410, 563):
+        for j in range(410, 610):
             if data[i, j] < 100:
                 hit("down")
                 return
 
     for i in range(300, 415):
-        for j in range(563, 650):
+        for j in range(600, 650):
             if data[i, j] < 100:
                 hit("up")
                 return
@@ -31,3 +31,4 @@ if __name__ == "__main__":
         image = ImageGrab.grab().convert('L')  
         data = image.load()
         isCollide(data)
+
